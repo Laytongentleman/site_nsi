52fa552ef4c8865193f6561da6bb993b38c325ba
Laytongentleman <nmtsiuol@gmail.com>
2021-04-20 22:55:40 +0200

    note_search

--- js/toggle_search.js
+++ js/toggle_search.js
@@ -3,3 +3,11 @@ var menuToggle = document.querySelector(".menu-toggle");
 menuToggle.addEventListener("click", function(e) {
   menu.classList.toggle("invisible");
 });
+
+
+var add = document.querySelector(".formadd");
+var addToggle = document.querySelector(".add");
+
+addToggle.addEventListener("click", function(e) {
+  add.classList.toggle("invisible");
+});
