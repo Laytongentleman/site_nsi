e50fa1c884ee3490914c2d4b7cc5448637e87732
Laytongentleman <nmtsiuol@gmail.com>
2021-05-04 12:32:24 +0200

    commit to main

--- js/audiobutton.js
+++ js/audiobutton.js
@@ -1,7 +1,10 @@
+"use strict";
 
-var x = document.getElementsByClassName("audio");
 
-var button = document.getElementsByClassName("audiobutton");
+var x = document.querySelector(".audio");
+
+var button = document.querySelector(".playbutton");
+var button_reload = document.querySelector(".reloadbutton");
 var play = true;
 
 function playAudio() {
@@ -12,13 +15,51 @@ function pauseAudio() {
   x.pause();
 }
 
-button.addEventListener('click', () => {
+function reloadAudio() {
+  x.pause();
+  x.currentTime = 0;
+  x.play();      
+  play = true;
+  console.log("reloaded");
+}
+
+button.addEventListener('click', function(e) {
+  console.log(play)
     if (play == true) {
         pauseAudio();
-        console.log(play)
+        p.src = '/img/play.png';
+        play = false;
         
     } else {
         playAudio();
+        p.src = '/img/pause.png';
+        play = true;
     }
-});
+})
 
+button_reload.addEventListener('click', function(e) {
+    reloadAudio();
+})
+
+
+
+const init = function(e) {
+  let btn = document.querySelector(".pont");
+  console.log(btn);
+  let audiotime = localStorage.getItem('audio-time')
+  x.currentTime = audiotime
+  btn.addEventListener('click', function() {
+  });
+};
+
+document.addEventListener('DOMContentLoaded', function(){
+  init();});
+ 
+  function myfun(){
+    localStorage.setItem('audio-time', x.currentTime)
+    console.log('hello');
+}
+
+window.onbeforeunload = function(){
+  myfun();
+};
